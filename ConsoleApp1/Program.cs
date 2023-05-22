516c3e458f086279c995b4fb8f170486b76b7111
Lautaro <lauti1070@hotmail.com.ar>
2023-05-22 13:23:15 -0300

    modi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,24 +10,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            while (true){
-                Console.Write("Ingrese un numero: ");
-                int num = 0;
-                try
-                {
-                    num = Convert.ToInt32(Console.ReadLine());
-                }
-                catch
-                {
-                    Console.WriteLine("El caracter ingresado no es valido");
-                    break;
-                }
+            //while (true){
+            //    Console.Write("Ingrese un numero: ");
+            //    int num = 0;
+            //    try
+            //    {
+            //        num = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //    catch
+            //    {
+            //        Console.WriteLine("El caracter ingresado no es valido");
+            //        break;
+            //    }
 
-                if (analizarNumero.analizarPrimo(num))
-                    Console.WriteLine("El numero es primo");
-                else
-                    Console.WriteLine("El numero no es primo");
+            //    if (analizarNumero.analizarPrimo(num))
+            //        Console.WriteLine("El numero es primo");
+            //    else
+            //        Console.WriteLine("El numero no es primo");
+            //}
+            int cant = 0;
+            for (int i = 9_999; i < 100_000; i++) 
+            {
+                if (analizarNumero.analizarPrimo(i))
+                {
+                    cant++;
+                    Console.WriteLine(i);
+                }
             }
+            Console.WriteLine("Cantidad de primos: " + cant);
         }
     }
 }
